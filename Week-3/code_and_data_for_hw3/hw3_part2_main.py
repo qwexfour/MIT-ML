abdf6e4401602f788f0afe03b3e485dfefd3551b
Dmitry Ryabtsev <dmitry.ryabtsev.96@gmail.com>
2022-10-18 01:04:10 +0600

    [Refactor] HW3 program was reorganized.
    Handling of separate data sets was put into separate functions.
    Options are provided to launch the work on specific data sets.

--- Week-3/code_and_data_for_hw3/hw3_part2_main.py
+++ Week-3/code_and_data_for_hw3/hw3_part2_main.py
@@ -19,133 +19,151 @@ this program. If not, see <http://www.gnu.org/licenses/>.
 # All the used assets should be downloaded from there too.
 
 import pdb
+import argparse
 import numpy as np
 import code_for_hw3_part2 as hw3
 
-#-------------------------------------------------------------------------------
-# Auto Data
-#-------------------------------------------------------------------------------
+def main():
+  args = parse_args()
+  if args.auto:
+    process_auto()
+  if args.review:
+    process_review()
+  if args.mnist:
+    process_mnist()
 
-# Returns a list of dictionaries.  Keys are the column names, including mpg.
-auto_data_all = hw3.load_auto_data('auto-mpg.tsv')
+def parse_args():
+  parser = argparse.ArgumentParser(description='I will perform tasks from HW3.')
+  parser.add_argument('--auto', action='store_true',
+                      help='work on auto data set')
+  parser.add_argument('--review', action='store_true',
+                      help='work on review data set')
+  parser.add_argument('--mnist', action='store_true',
+                      help='work on mnist data set')
+  return parser.parse_args()
 
-# The choice of feature processing for each feature, mpg is always raw and
-# does not need to be specified.  Other choices are hw3.standard and hw3.one_hot.
-# 'name' is not numeric and would need a different encoding.
-feature_sets = [[('cylinders', hw3.raw),
-                 ('displacement', hw3.raw),
-                 ('horsepower', hw3.raw),
-                 ('weight', hw3.raw),
-                 ('acceleration', hw3.raw),
-                 ## Drop model_year by default
-                 ## ('model_year', hw3.raw),
-                 ('origin', hw3.raw)],
-                [('cylinders', hw3.one_hot),
-                 ('displacement', hw3.standard),
-                 ('horsepower', hw3.standard),
-                 ('weight', hw3.standard),
-                 ('acceleration', hw3.standard),
-                 ## Drop model_year by default
-                 ## ('model_year', hw3.standard),
-                 ('origin', hw3.one_hot)]]
-Ts = [1, 10, 50]
+def process_auto():
+  #-------------------------------------------------------------------------------
+  # Auto Data
+  #-------------------------------------------------------------------------------
 
-if False:                               # set to True to see histograms
-  import matplotlib.pyplot as plt
-  for feat in range(auto_data.shape[0]):
-    print('Feature', feat, features[feat][0])
-    # Plot histograms in one window, different colors
-    plt.hist(auto_data[feat,auto_labels[0,:] > 0])
-    plt.hist(auto_data[feat,auto_labels[0,:] < 0])
-    plt.show()
-    # Plot histograms in two windows, different colors
-    fig,(a1,a2) = plt.subplots(nrows=2)
-    a1.hist(auto_data[feat,auto_labels[0,:] > 0])
-    a2.hist(auto_data[feat,auto_labels[0,:] < 0])
-    plt.show()
+  # Returns a list of dictionaries.  Keys are the column names, including mpg.
+  auto_data_all = hw3.load_auto_data('auto-mpg.tsv')
 
-#-------------------------------------------------------------------------------
-# Analyze auto data
-#-------------------------------------------------------------------------------
+  # The choice of feature processing for each feature, mpg is always raw and
+  # does not need to be specified.  Other choices are hw3.standard and hw3.one_hot.
+  # 'name' is not numeric and would need a different encoding.
+  feature_sets = [[('cylinders', hw3.raw),
+                   ('displacement', hw3.raw),
+                   ('horsepower', hw3.raw),
+                   ('weight', hw3.raw),
+                   ('acceleration', hw3.raw),
+                   ## Drop model_year by default
+                   ## ('model_year', hw3.raw),
+                   ('origin', hw3.raw)],
+                  [('cylinders', hw3.one_hot),
+                   ('displacement', hw3.standard),
+                   ('horsepower', hw3.standard),
+                   ('weight', hw3.standard),
+                   ('acceleration', hw3.standard),
+                   ## Drop model_year by default
+                   ## ('model_year', hw3.standard),
+                   ('origin', hw3.one_hot)]]
+  Ts = [1, 10, 50]
 
-# Your code here to process the auto data
+  #-------------------------------------------------------------------------------
+  # Analyze auto data
+  #-------------------------------------------------------------------------------
 
-for f_set_idx, features in enumerate(feature_sets):
-  for T in Ts:
-    # Construct the standard data and label arrays
-    auto_data, auto_labels = hw3.auto_data_and_labels(auto_data_all, features)
-    ptron_score = hw3.xval_learning_alg(hw3.perceptron, auto_data,
-                                        auto_labels, 10, T)
-    av_ptron_score = hw3.xval_learning_alg(hw3.averaged_perceptron, auto_data,
-                                           auto_labels, 10, T)
-    print(f'Analysis for auto data for feature set {f_set_idx+1} and T = {T}:')
-    print('  auto data and labels shape', auto_data.shape, auto_labels.shape)
-    print('  Perceptron score is ', ptron_score)
-    print('  Averaged perceptron score is ', av_ptron_score)
+  # Your code here to process the auto data
 
-auto_data, auto_labels = hw3.auto_data_and_labels(auto_data_all, feature_sets[1])
-auto_theta = hw3.averaged_perceptron(auto_data, auto_labels, {'T' : 1})
-print('The best separator using averaged perceptron, T = 1, the first feature '
-      'set:')
-print(*auto_theta)
+  for f_set_idx, features in enumerate(feature_sets):
+    for T in Ts:
+      # Construct the standard data and label arrays
+      auto_data, auto_labels = hw3.auto_data_and_labels(auto_data_all, features)
+      ptron_score = hw3.xval_learning_alg(hw3.perceptron, auto_data,
+                                          auto_labels, 10, T)
+      av_ptron_score = hw3.xval_learning_alg(hw3.averaged_perceptron, auto_data,
+                                             auto_labels, 10, T)
+      print(f'Analysis for auto data for feature set {f_set_idx+1} and T = {T}:')
+      print('  auto data and labels shape', auto_data.shape, auto_labels.shape)
+      print('  Perceptron score is ', ptron_score)
+      print('  Averaged perceptron score is ', av_ptron_score)
 
-#-------------------------------------------------------------------------------
-# Review Data
-#-------------------------------------------------------------------------------
+  auto_data, auto_labels = hw3.auto_data_and_labels(auto_data_all, feature_sets[1])
+  auto_theta = hw3.averaged_perceptron(auto_data, auto_labels, {'T' : 1})
+  print('The best separator using averaged perceptron, T = 1, the first feature '
+        'set:')
+  print(*auto_theta)
 
-# Returns lists of dictionaries.  Keys are the column names, 'sentiment' and 'text'.
-# The train data has 10,000 examples
-review_data = hw3.load_review_data('reviews.tsv')
+def process_review():
+  #-------------------------------------------------------------------------------
+  # Review Data
+  #-------------------------------------------------------------------------------
 
-# Lists texts of reviews and list of labels (1 or -1)
-review_texts, review_label_list = zip(*((sample['text'], sample['sentiment']) for sample in review_data))
+  # Returns lists of dictionaries.  Keys are the column names, 'sentiment' and 'text'.
+  # The train data has 10,000 examples
+  review_data = hw3.load_review_data('reviews.tsv')
 
-# The dictionary of all the words for "bag of words"
-dictionary = hw3.bag_of_words(review_texts)
+  # Lists texts of reviews and list of labels (1 or -1)
+  review_texts, review_label_list = zip(*((sample['text'], sample['sentiment']) for sample in review_data))
 
-# The standard data arrays for the bag of words
-review_bow_data = hw3.extract_bow_feature_vectors(review_texts, dictionary)
-review_labels = hw3.rv(review_label_list)
-print('review_bow_data and labels shape', review_bow_data.shape, review_labels.shape)
+  # The dictionary of all the words for "bag of words"
+  dictionary = hw3.bag_of_words(review_texts)
 
-#-------------------------------------------------------------------------------
-# Analyze review data
-#-------------------------------------------------------------------------------
+  # The standard data arrays for the bag of words
+  review_bow_data = hw3.extract_bow_feature_vectors(review_texts, dictionary)
+  review_labels = hw3.rv(review_label_list)
+  print('review_bow_data and labels shape', review_bow_data.shape, review_labels.shape)
 
-# Your code here to process the review data
+  #-------------------------------------------------------------------------------
+  # Analyze review data
+  #-------------------------------------------------------------------------------
 
-#-------------------------------------------------------------------------------
-# MNIST Data
-#-------------------------------------------------------------------------------
+  # Your code here to process the review data
 
-"""
-Returns a dictionary formatted as follows:
-{
-    0: {
-        "images": [(m by n image), (m by n image), ...],
-        "labels": [0, 0, ..., 0]
-    },
-    1: {...},
-    ...
-    9
-}
-Where labels range from 0 to 9 and (m, n) images are represented
-by arrays of floats from 0 to 1
-"""
-mnist_data_all = hw3.load_mnist_data(range(10))
+def process_mnist():
+  #-------------------------------------------------------------------------------
+  # MNIST Data
+  #-------------------------------------------------------------------------------
 
-print('mnist_data_all loaded. shape of single images is', mnist_data_all[0]["images"][0].shape)
+  """
+  Returns a dictionary formatted as follows:
+  {
+      0: {
+          "images": [(m by n image), (m by n image), ...],
+          "labels": [0, 0, ..., 0]
+      },
+      1: {...},
+      ...
+      9
+  }
+  Where labels range from 0 to 9 and (m, n) images are represented
+  by arrays of floats from 0 to 1
+  """
+  mnist_data_all = hw3.load_mnist_data(range(10))
 
-# HINT: change the [0] and [1] if you want to access different images
-d0 = mnist_data_all[0]["images"]
-d1 = mnist_data_all[1]["images"]
-y0 = np.repeat(-1, len(d0)).reshape(1,-1)
-y1 = np.repeat(1, len(d1)).reshape(1,-1)
+  print('mnist_data_all loaded. shape of single images is', mnist_data_all[0]["images"][0].shape)
 
-# data goes into the feature computation functions
-data = np.vstack((d0, d1))
-# labels can directly go into the perceptron algorithm
-labels = np.vstack((y0.T, y1.T)).T
+  # HINT: change the [0] and [1] if you want to access different images
+  d0 = mnist_data_all[0]["images"]
+  d1 = mnist_data_all[1]["images"]
+  y0 = np.repeat(-1, len(d0)).reshape(1,-1)
+  y1 = np.repeat(1, len(d1)).reshape(1,-1)
+
+  # data goes into the feature computation functions
+  data = np.vstack((d0, d1))
+  # labels can directly go into the perceptron algorithm
+  labels = np.vstack((y0.T, y1.T)).T
+
+  # use this function to evaluate accuracy
+  #acc = hw3.get_classification_accuracy(raw_mnist_features(data), labels)
+
+  #-------------------------------------------------------------------------------
+  # Analyze MNIST data
+  #-------------------------------------------------------------------------------
+
+  # Your code here to process the MNIST data
 
 def raw_mnist_features(x):
   """
@@ -186,12 +204,5 @@ def top_bottom_features(x):
   """
   raise Exception("modify me!")
 
-# use this function to evaluate accuracy
-#acc = hw3.get_classification_accuracy(raw_mnist_features(data), labels)
-
-#-------------------------------------------------------------------------------
-# Analyze MNIST data
-#-------------------------------------------------------------------------------
-
-# Your code here to process the MNIST data
-
+if __name__ == "__main__":
+  main()
