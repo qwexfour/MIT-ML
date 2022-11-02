6c4ea6afebbdbe371c9f0571b77e9d90dff7a00b
Dmitry Ryabtsev <dmitry.ryabtsev.96@gmail.com>
2022-11-02 15:34:05 +0600

    [Feature] SVM objective function gradient routine implementation.
    A routine that calculates SVM objective function gradient in a provided
    point was added to svm module.

--- Week-4/svm.py
+++ Week-4/svm.py
@@ -63,6 +63,104 @@ def svm_obj(x, y, th, th0, lam):
   """
   return np.mean(hinge_loss(x, y, th, th0)) + lam * np.dot(th.T, th)[0, 0]
 
+# Returns the gradient of hinge(v) with respect to v.
+def d_hinge(v):
+  """Returns the derivative of hinge(v).
+
+  Parameters:
+    v - a row vector of alternative hinge scalar inputs.
+  Returns a row vector of hinge derivatives corresponding to the provided
+  values.
+  """
+  return np.where(v < 1, -1, 0)
+
+# Returns the gradient of hinge_loss(x, y, th, th0) with respect to th
+def d_hinge_loss_th(x, y, th, th0):
+  """Calculates hinge loss gradient for every provided labeled data point
+  with respect to theta parameter in th point.
+
+  Parameters:
+    x - n data points in d dimensions ([d x n] numpy array of numbers);
+    y - data labels ([1 x n] numpy array of elements in {+1, -1}, or a
+        single number +1 or -1 for n == 1);
+    th - hyperplane theta parameter ([d x 1] numpy array of numbers);
+    th0 - hyperplane theta_0 parameter ([1 x 1] numpy array of numbers,
+          or a single number).
+  Returns row vector of gradients for each point, which gives [d x n] matrix.
+  """
+  kinda_margin = y * hp.substitute(x, th, th0)
+  return d_hinge(kinda_margin) * y * x
+
+# Returns the gradient of hinge_loss(x, y, th, th0) with respect to th0
+def d_hinge_loss_th0(x, y, th, th0):
+  """Calculates hinge loss partial derivative for every provided labeled data
+  point with respect to theta_0 parameter in th0 point.
+
+  Parameters:
+    x - n data points in d dimensions ([d x n] numpy array of numbers);
+    y - data labels ([1 x n] numpy array of elements in {+1, -1}, or a
+        single number +1 or -1 for n == 1);
+    th - hyperplane theta parameter ([d x 1] numpy array of numbers);
+    th0 - hyperplane theta_0 parameter ([1 x 1] numpy array of numbers,
+          or a single number).
+  Returns row vector of partial derivatives for each point ([1 x n] matrix).
+  """
+  kinda_margin = y * hp.substitute(x, th, th0)
+  return d_hinge(kinda_margin) * y
+
+# Returns the gradient of svm_obj(x, y, th, th0) with respect to th
+def d_svm_obj_th(x, y, th, th0, lam):
+  """Calculates SVM objective function gradient with respect to theta
+  parameter in th point.
+
+  Parameters:
+    x - n data points in d dimensions ([d x n] numpy array of numbers);
+    y - data labels ([1 x n] numpy array of elements in {+1, -1}, or a
+        single number +1 or -1 for n == 1);
+    th - hyperplane theta parameter ([d x 1] numpy array of numbers);
+    th0 - hyperplane theta_0 parameter ([1 x 1] numpy array of numbers,
+          or a single number);
+    lam - regularization parameter, a number.
+  Returns the gradient ([d x 1] numpy array).
+  """
+  avg = np.mean(d_hinge_loss_th(x, y, th, th0), axis=1, keepdims=True)
+  return avg + 2 * lam * th
+
+# Returns the gradient of svm_obj(x, y, th, th0) with respect to th0
+def d_svm_obj_th0(x, y, th, th0, lam):
+  """Calculates SVM objective function partial derivative with respect to
+  theta_0 parameter in th0 point.
+
+  Parameters:
+    x - n data points in d dimensions ([d x n] numpy array of numbers);
+    y - data labels ([1 x n] numpy array of elements in {+1, -1}, or a
+        single number +1 or -1 for n == 1);
+    th - hyperplane theta parameter ([d x 1] numpy array of numbers);
+    th0 - hyperplane theta_0 parameter ([1 x 1] numpy array of numbers,
+          or a single number);
+    lam - regularization parameter, a number.
+  Returns the partial derivative ([1 x 1] numpy array).
+  """
+  return np.mean(d_hinge_loss_th0(x, y, th, th0), axis=1, keepdims=True)
+
+# Returns the full gradient as a single vector (which includes both th, th0)
+def svm_obj_grad(x, y, th, th0, lam):
+  """Calculates SVM objective function gradient with respect to theta
+  and theta_0 parameters in (th, th0) point.
+
+  Parameters:
+    x - n data points in d dimensions ([d x n] numpy array of numbers);
+    y - data labels ([1 x n] numpy array of elements in {+1, -1}, or a
+        single number +1 or -1 for n == 1);
+    th - hyperplane theta parameter ([d x 1] numpy array of numbers);
+    th0 - hyperplane theta_0 parameter ([1 x 1] numpy array of numbers,
+          or a single number);
+    lam - regularization parameter, a number.
+  Returns the gradient ([d+1 x 1] numpy array).
+  """
+  return np.vstack((d_svm_obj_th(x, y, th, th0, lam),
+                    d_svm_obj_th0(x, y, th, th0, lam)))
+
 def _super_simple_separable_svm_obj_test():
   x_1 = np.array([[2, 3, 9, 12],
                 [5, 2, 6, 5]])
@@ -78,8 +176,37 @@ def _super_simple_separable_svm_obj_test():
   ans = svm_obj(x_1, y_1, th1, th1_0, 0.0)
   print(ans)
 
+def _svm_obj_grad_test():
+  X1 = np.array([[1, 2, 3, 9, 10]])
+  y1 = np.array([[1, 1, 1, -1, -1]])
+  th1, th10 = np.array([[-0.31202807]]), np.array([[1.834     ]])
+  X2 = np.array([[2, 3, 9, 12],
+                 [5, 2, 6, 5]])
+  y2 = np.array([[1, -1, 1, -1]])
+  th2, th20=np.array([[ -3.,  15.]]).T, np.array([[ 2.]])
+
+  assert d_hinge(np.array([[ 71.]])).tolist() == [[0]]
+  assert d_hinge(np.array([[ -23.]])).tolist() == [[-1]]
+  assert d_hinge(np.array([[ 71, -23.]])).tolist() == [[0, -1]]
+
+  assert d_hinge_loss_th(X2[:,0:1], y2[:,0:1], th2, th20).tolist() == [[0], [0]]
+  assert d_hinge_loss_th(X2, y2, th2, th20).tolist() == \
+         [[0, 3, 0, 12], [0, 2, 0, 5]]
+  assert d_hinge_loss_th0(X2[:,0:1], y2[:,0:1], th2, th20).tolist() == [[0]]
+  assert d_hinge_loss_th0(X2, y2, th2, th20).tolist() == [[0, 1, 0, 1]]
+
+  assert d_svm_obj_th(X2[:,0:1], y2[:,0:1], th2, th20, 0.01).tolist() == \
+         [[-0.06], [0.3]]
+  assert d_svm_obj_th(X2, y2, th2, th20, 0.01).tolist() == [[3.69], [2.05]]
+  assert d_svm_obj_th0(X2[:,0:1], y2[:,0:1], th2, th20, 0.01).tolist() == [[0.0]]
+  assert d_svm_obj_th0(X2, y2, th2, th20, 0.01).tolist() == [[0.5]]
+
+  assert svm_obj_grad(X2, y2, th2, th20, 0.01).tolist() == [[3.69], [2.05], [0.5]]
+  assert svm_obj_grad(X2[:,0:1], y2[:,0:1], th2, th20, 0.01).tolist() == \
+         [[-0.06], [0.3], [0.0]]
+
 def _main():
-  pass
+  _svm_obj_grad_test()
 
 if __name__ == "__main__":
   _main()
